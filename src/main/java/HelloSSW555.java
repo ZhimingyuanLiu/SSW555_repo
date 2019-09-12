d02b7cc73b18766c8c682c5309e2e6f7bad346bb
bestksl <kslup7@gmail.com>
2019-09-12 19:30:29 -0400

    Revert "fix project issues"

--- src/main/java/HelloSSW555.java
+++ src/main/java/HelloSSW555.java
@@ -1,10 +1,9 @@
 /**
  * @author HaoxuanLi  Github:bestksl
- * @version created date：2019-09-12 16:44
+ * @version created date：2019-09-12 16:31
  */
 public class HelloSSW555 {
     public static void main(String[] args) {
         System.out.println("hello SSW555!");
     }
 }
-
